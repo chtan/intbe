1bbf042fb13632642fc6dc29fa7f9f5917435725
chtan <chtan@suss.edu.sg>
2025-06-06 10:37:02 +0800

    added token-cleanup code; update consumers.py to handle authorization logic; added django-extensions

--- home/consumers.py
+++ home/consumers.py
@@ -1,6 +1,10 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from users.authentication import MongoJWTAuthentication, AnonymousTokenAuthentication
+from rest_framework.request import Request
+from rest_framework.test import APIRequestFactory
+from rest_framework.exceptions import AuthenticationFailed
+from mongoengine.connection import get_db
 
 """
 For logged-in user:
@@ -36,7 +40,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         query_string = self.scope["query_string"].decode()
         params = dict(x.split("=") for x in query_string.split("&") if "=" in x)
-        token = params.get("token")
+        token = params.get("token") # this is access token
+        taskid = params.get("taskid")
 
         # Currently, I have 3 websockets on angular side:
         # 1- old one
@@ -55,20 +60,80 @@ class ChatConsumer(AsyncWebsocketConsumer):
         #   - url without (access) token has task token. authenticate this.
         #     Then allow only messaging if target is coordinator.
 
-        #print("@@@@@", token)
+        #print("@@@@@", token, taskid)
 
-        if token:
+        # Either authenticated user or anonymous user access this.
+        # Either task_tokens or coordinator should be used later to control messaging requests.
+        user = None
+        anon_user = None
+
+        self.username = self.scope["url_route"]["kwargs"]["username"]
+        self.group_name = f"user_{self.username}"  # Unique channel name for the user
+
+        #
+        # Case: Authenticated user
+        #
+        if token: # if access token is present, indicating logged-in user
             jwt_auth = MongoJWTAuthentication()
             try:
                 validated_token = jwt_auth.get_validated_token(token)
                 user = jwt_auth.get_user(validated_token)
-                #print("444444", user)
+                #print("444444", user, user.id)
             except Exception as e:
-                #print("#@#$@#$", str(e))
-                pass
+                print(str(e))
+                #pass
 
-        self.username = self.scope["url_route"]["kwargs"]["username"]
-        self.group_name = f"user_{self.username}"  # Unique channel name for the user
+        # Obtain the list of tokens for this logged-in user and taskid
+        self.task_tokens = []
+        if user is not None:
+            collection = get_db()["task_links"]
+
+            # Query filter
+            query = {
+                "uid": self.username,
+                "taskid": taskid
+            }
+
+            # Only return the `tasklink` field (1 = include, 0 = exclude `_id`)
+            projection = {
+                "tasklink": 1,
+                "_id": 0
+            }
+
+            # Perform query
+            results = collection.find(query, projection)
+
+            # Extract tasklink into a list
+            self.task_tokens = [doc["tasklink"] for doc in results if "tasklink" in doc]
+
+
+        #
+        # Case: Anonymous user
+        #
+        if not token: # if access token is not present, indicating anonymous user
+            factory = APIRequestFactory()
+            request = factory.get('/fake-url/', HTTP_X_ANONYMOUS_TOKEN=self.username)  # header key is capitalized with HTTP_ prefix
+            auth = AnonymousTokenAuthentication()
+
+            try:
+                anon_user, _ = auth.authenticate(Request(request))
+                if anon_user and getattr(anon_user, "is_anonymous_token", False):
+                    print("Anonymous user authenticated:", anon_user.token)
+                else:
+                    print("Token invalid or user not anonymous.")
+            except AuthenticationFailed as e:
+                print("Authentication failed:", str(e))
+
+        # Get the coordinator for the anonymous user
+        self.coordinator = None
+        if anon_user is not None:
+            collection = get_db()["task_links"]
+            doc = collection.find_one({"tasklink": self.username}, {"uid": 1})  # Only return 'uid' field
+            if doc and 'uid' in doc:
+                self.coordinator = doc['uid']
+
+
+
 
         # Join the group
         await self.channel_layer.group_add(self.group_name, self.channel_name)
@@ -79,7 +144,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Notify the user
         await self.send(text_data=json.dumps({"message": f"{self.username} connected!!"}))
 
-        #print("CONN", self.username, self.group_name, self.channel_name, token)
+
+        # Custom checking...
+        print("CONN", self.username, self.group_name, self.channel_name, token, self.coordinator, self.task_tokens)
+
 
     async def disconnect(self, close_code):
         # Leave the group
